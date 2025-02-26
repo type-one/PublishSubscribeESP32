41d18b8b13a39687fb1bb25cc1e3f90a13c055f7
Laurent Lardinois <19912792+type-one@users.noreply.github.com>
2025-02-26 17:35:07 +0100

    docs: add doxygen doc to ring_buffer

--- main/tools/ring_buffer.hpp
+++ main/tools/ring_buffer.hpp
@@ -1,3 +1,16 @@
+/**
+ * @file ring_buffer.hpp
+ * @brief A header file defining a ring buffer class template with a fixed capacity.
+ * 
+ * This file contains the definition of the ring_buffer class template, which provides
+ * a circular buffer implementation with a fixed capacity. The ring buffer supports
+ * operations such as push, pop, front, back, and various utility functions.
+ * 
+ * @author Laurent Lardinois
+ * 
+ * @date January 2025
+ */
+
 //-----------------------------------------------------------------------------//
 // C++ Publish/Subscribe Pattern - Spare time development for fun              //
 // (c) 2025 Laurent Lardinois https://be.linkedin.com/in/laurentlardinois      //
@@ -34,10 +47,23 @@
 
 namespace tools
 {
+    /**
+     * @brief A class representing a ring buffer with a fixed capacity.
+     *
+     * This class provides a circular buffer implementation with a fixed capacity.
+     *
+     * @tparam T The type of elements stored in the ring buffer.
+     * @tparam Capacity The maximum number of elements the ring buffer can hold.
+     */
     template <typename T, std::size_t Capacity>
     class ring_buffer
     {
     public:
+        /**
+         * @brief A struct indicating whether the ring buffer is thread-safe.
+         *
+         * This struct can be specialized to enable thread-safe operations in derived classes.
+         */
         struct thread_safe
         {
             static constexpr bool value = false;
@@ -46,6 +72,14 @@ namespace tools
         ring_buffer() = default;
         ~ring_buffer() = default;
 
+        /**
+         * @brief Copy constructor for the ring_buffer class.
+         *
+         * This constructor initializes a new ring_buffer object by copying the state
+         * from another ring_buffer object.
+         *
+         * @param other The ring_buffer object to copy from.
+         */
         ring_buffer(const ring_buffer& other)
             : m_ring_buffer { other.m_ring_buffer }
             , m_push_index { other.m_push_index }
@@ -55,6 +89,14 @@ namespace tools
         {
         }
 
+        /**
+         * @brief Move constructor for the ring_buffer class.
+         *
+         * This constructor initializes a ring_buffer object by transferring ownership
+         * of the resources from another ring_buffer object.
+         *
+         * @param other The ring_buffer object to move from.
+         */
         ring_buffer(ring_buffer&& other) noexcept
             : m_ring_buffer { std::move(other.m_ring_buffer) }
             , m_push_index { std::move(other.m_push_index) }
@@ -64,6 +106,15 @@ namespace tools
         {
         }
 
+        /**
+         * @brief Copy assignment operator for the ring_buffer class.
+         *
+         * This operator assigns the contents of another ring_buffer instance to this instance.
+         * It performs a deep copy of the internal buffer and indices.
+         *
+         * @param other The ring_buffer instance to copy from.
+         * @return A reference to this ring_buffer instance.
+         */
         ring_buffer& operator=(const ring_buffer& other)
         {
             if (this != &other)
@@ -78,6 +129,16 @@ namespace tools
             return *this;
         }
 
+        /**
+         * @brief Move assignment operator for the ring_buffer class.
+         *
+         * This operator moves the contents of another ring_buffer instance to this instance.
+         * It ensures that the current instance is not the same as the other instance before
+         * performing the move operations.
+         *
+         * @param other The ring_buffer instance to move from.
+         * @return A reference to this ring_buffer instance.
+         */
         ring_buffer& operator=(ring_buffer&& other) noexcept
         {
             if (this != &other)
@@ -92,6 +153,11 @@ namespace tools
             return *this;
         }
 
+        /**
+         * @brief Pushes an element into the ring buffer.
+         *
+         * @param elem The element to be pushed into the ring buffer.
+         */
         void push(const T& elem)
         {
             m_ring_buffer.at(m_push_index) = elem;
@@ -100,6 +166,11 @@ namespace tools
             ++m_size;
         }
 
+        /**
+         * @brief Inserts in place an element into the ring buffer at the current push index.
+         *
+         * @param elem The element to be inserted into the ring buffer.
+         */
         void emplace(T&& elem)
         {
             m_ring_buffer.at(m_push_index) = std::move(elem);
@@ -108,32 +179,63 @@ namespace tools
             ++m_size;
         }
 
+        /**
+         * @brief Removes the oldest element from the ring buffer.
+         */
         void pop()
         {
             m_pop_index = next_index(m_pop_index);
             --m_size;
         }
 
+        /**
+         * @brief Retrieves the front element of the ring buffer.
+         *
+         * This method returns the element at the front of the ring buffer
+         * without removing it.
+         *
+         * @return The element at the front of the ring buffer.
+         */
         [[nodiscard]] T front() const
         {
             return m_ring_buffer.at(m_pop_index);
         }
 
+        /**
+         * @brief Retrieves the last element in the ring buffer.
+         *
+         * @return The last element of type T in the ring buffer.
+         */
         [[nodiscard]] T back() const
         {
             return m_ring_buffer.at(m_last_index);
         }
 
+        /**
+         * @brief Checks if the ring buffer is empty.
+         *
+         * This method returns true if the ring buffer has no elements
+         *
+         * @return true if the ring buffer is empty, false otherwise.
+         */
         [[nodiscard]] bool empty() const
         {
             return m_push_index == m_pop_index;
         }
 
+        /**
+         * @brief Checks if the ring buffer is full.
+         *
+         * @return true if the ring buffer is full, false otherwise.
+         */
         [[nodiscard]] bool full() const
         {
             return next_index(m_push_index) == m_pop_index;
         }
 
+        /**
+         * @brief Clears the ring buffer, resetting all indices and size to zero.
+         */
         void clear()
         {
             m_push_index = 0U;
@@ -143,26 +245,62 @@ namespace tools
             m_ring_buffer = {};
         }
 
+        /**
+         * @brief Returns the size of the ring buffer.
+         *
+         * @return std::size_t The current size of the ring buffer.
+         */
         [[nodiscard]] std::size_t size() const
         {
             return m_size;
         }
 
+        /**
+         * @brief Get the capacity of the ring buffer.
+         *
+         * @return constexpr std::size_t The capacity of the ring buffer.
+         */
         [[nodiscard]] constexpr std::size_t capacity() const
         {
             return Capacity;
         }
 
+        /**
+         * @brief Accesses the element at the specified index in the ring buffer.
+         *
+         * @param index The index of the element to access.
+         * @return T& Reference to the element at the specified index.
+         */
         T& operator[](std::size_t index)
         {
             return m_ring_buffer.at(next_step_index(m_pop_index, index));
         };
 
     private:
+        /**
+         * @brief Computes the next index in a circular buffer.
+         *
+         * This function calculates the next index in a circular buffer
+         * based on the current index and the buffer's capacity.
+         *
+         * @param index The current index in the buffer.
+         * @return The next index in the circular buffer.
+         */
         static constexpr std::size_t next_index(std::size_t index)
         {
             return ((index + 1U) % Capacity);
         }
+
+        /**
+         * @brief Calculates the next index in the ring buffer given a current index and a step.
+         *
+         * This function computes the next index by adding the step to the current index
+         * and taking the result modulo the buffer capacity.
+         *
+         * @param index The current index in the ring buffer.
+         * @param step The number of steps to move forward in the ring buffer.
+         * @return The next index in the ring buffer after moving forward by the specified step.
+         */
         static constexpr std::size_t next_step_index(std::size_t index, std::size_t step)
         {
             return ((index + step) % Capacity);
