2ac5618060f2c5e14728cb5672e946f61927282c
Laurent Lardinois <19912792+type-one@users.noreply.github.com>
2025-02-26 19:34:53 +0100

    docs: add doxygen doc to sync_queue

--- main/tools/sync_queue.hpp
+++ main/tools/sync_queue.hpp
@@ -1,3 +1,15 @@
+/**
+ * @file sync_queue.hpp
+ * @brief A thread-safe queue implementation for the Publish/Subscribe Pattern.
+ *
+ * This file contains the definition of the sync_queue class, which provides a thread-safe
+ * queue with various methods to manipulate the queue. It is part of the Publish/Subscribe
+ * Pattern project for ESP32.
+ *
+ * @author Laurent Lardinois
+ * @date January 2025
+ */
+
 //-----------------------------------------------------------------------------//
 // C++ Publish/Subscribe Pattern - Spare time development for fun              //
 // (c) 2025 Laurent Lardinois https://be.linkedin.com/in/laurentlardinois      //
@@ -36,6 +48,14 @@
 
 namespace tools
 {
+    /**
+     * @brief A thread-safe queue implementation.
+     *
+     * This class provides a thread-safe queue with various methods to manipulate the queue.
+     * It inherits from non_copyable to prevent copying and moving.
+     *
+     * @tparam T The type of elements stored in the queue.
+     */
     template <typename T>
     class sync_queue : public non_copyable // NOLINT inherits from non copyable/non movable
     {
@@ -47,60 +67,135 @@ namespace tools
             static constexpr bool value = true;
         };
 
+        /**
+         * @brief Pushes an element into the queue.
+         *
+         * This method adds a new element to the end of the queue in a thread-safe manner.
+         *
+         * @param elem The element to be pushed into the queue.
+         */
         void push(const T& elem)
         {
             std::lock_guard guard(m_mutex);
             m_queue.push(elem);
         }
 
+        /**
+         * @brief Adds a new element to the queue.
+         *
+         * This method locks the mutex to ensure thread safety and then
+         * adds a new element to the queue using perfect forwarding.
+         *
+         * @param elem The element to be added to the queue.
+         */
         void emplace(T&& elem)
         {
             std::lock_guard guard(m_mutex);
             m_queue.emplace(elem);
         }
 
+        /**
+         * @brief Removes the front element from the queue.
+         *
+         * This method locks the mutex to ensure thread safety and then removes the front element from the queue.
+         */
         void pop()
         {
             std::lock_guard guard(m_mutex);
             m_queue.pop();
         }
 
+        /**
+         * @brief Retrieves the front element of the queue.
+         *
+         * This method returns the front element of the queue in a thread-safe manner.
+         *
+         * @return The front element of the queue.
+         */
         T front()
         {
             std::lock_guard guard(m_mutex);
             return m_queue.front();
         }
 
+        /**
+         * @brief Retrieves the last element in the queue.
+         *
+         * This method returns the last element in the queue. It uses a lock guard to ensure
+         * thread safety while accessing the queue.
+         *
+         * @return The last element in the queue.
+         */
         T back()
         {
             std::lock_guard guard(m_mutex);
             return m_queue.back();
         }
 
+        /**
+         * @brief Checks if the queue is empty.
+         *
+         * This method acquires a lock on the mutex to ensure thread safety
+         * and then checks if the underlying queue is empty.
+         *
+         * @return true if the queue is empty, false otherwise.
+         */
         bool empty()
         {
             std::lock_guard guard(m_mutex);
             return m_queue.empty();
         }
 
+        /**
+         * @brief Returns the number of elements in the queue.
+         *
+         * This method acquires a lock on the mutex to ensure thread safety
+         * while accessing the underlying queue's size.
+         *
+         * @return The number of elements in the queue.
+         */
         std::size_t size()
         {
             std::lock_guard guard(m_mutex);
             return m_queue.size();
         }
 
+        /**
+         * @brief Pushes an element into the queue in an interrupt service routine (ISR) safe manner.
+         *
+         * This function uses an ISR lock guard to ensure that the push operation is thread-safe
+         * and can be safely called from an ISR context.
+         *
+         * @param elem The element to be pushed into the queue.
+         */
         void isr_push(const T& elem)
         {
             tools::isr_lock_guard<tools::critical_section> guard(m_mutex);
             m_queue.push(elem);
         }
 
+        /**
+         * @brief Inserts an element into the queue in an interrupt-safe manner.
+         *
+         * This function uses a lock guard to ensure that the insertion operation
+         * is safe to perform within an interrupt service routine (ISR).
+         *
+         * @param elem The element to be inserted into the queue.
+         */
         void isr_emplace(T&& elem)
         {
             tools::isr_lock_guard<tools::critical_section> guard(m_mutex);
             m_queue.emplace(elem);
         }
 
+        /**
+         * @brief Returns the size of the queue in an interrupt service routine (ISR) safe manner.
+         *
+         * This function acquires a lock using an ISR-safe lock guard to ensure that the size of the queue
+         * is read in a thread-safe manner, even when called from an ISR.
+         *
+         * @return The size of the queue.
+         */
         std::size_t isr_size()
         {
             tools::isr_lock_guard<tools::critical_section> guard(m_mutex);
