42306229e74d3c5c3d108c6723d50246b0cc7db6
Laurent Lardinois <19912792+type-one@users.noreply.github.com>
2025-02-26 15:36:26 +0100

    docs: add doxygen doc to async_observer

--- main/tools/async_observer.hpp
+++ main/tools/async_observer.hpp
@@ -1,3 +1,10 @@
+/**
+ * @file async_observer.hpp
+ * @brief A header file for the async_observer class providing asynchronous observation capabilities.
+ * @author Laurent Lardinois
+ * @date January 2025
+ */
+
 //-----------------------------------------------------------------------------//
 // C++ Publish/Subscribe Pattern - Spare time development for fun              //
 // (c) 2025 Laurent Lardinois https://be.linkedin.com/in/laurentlardinois      //
@@ -39,7 +46,14 @@
 
 namespace tools
 {
-
+    /**
+     * @brief A class that provides asynchronous observation capabilities.
+     *
+     * This class inherits from sync_observer and allows events to be queued and processed asynchronously.
+     *
+     * @tparam Topic The type of the topic associated with the events.
+     * @tparam Evt The type of the event data.
+     */
     template <typename Topic, typename Evt>
     class async_observer : public sync_observer<Topic, Evt> // NOLINT inherits from non copyable and non movable class
     {
@@ -47,6 +61,15 @@ namespace tools
         async_observer() = default;
         ~async_observer() override = default;
 
+        /**
+         * @brief Informs the observer of a new event.
+         *
+         * This method pushes the event information onto the event queue and signals the wakeable object.
+         *
+         * @param topic The topic associated with the event.
+         * @param event The event data.
+         * @param origin The origin of the event.
+         */
         void inform(const Topic& topic, const Evt& event, const std::string& origin) override
         {
             m_evt_queue.push({ topic, event, origin });
@@ -55,6 +78,13 @@ namespace tools
 
         using event_entry = std::tuple<Topic, Evt, std::string>;
 
+        /**
+         * @brief Pops all events from the event queue.
+         *
+         * This function removes all events from the internal event queue and returns them as a vector.
+         *
+         * @return std::vector<event_entry> A vector containing all the events that were in the queue.
+         */
         std::vector<event_entry> pop_all_events()
         {
             std::vector<event_entry> events;
@@ -68,6 +98,13 @@ namespace tools
             return events;
         }
 
+        /**
+         * @brief Pops the first event from the event queue.
+         *
+         * This method retrieves and removes the first event from the event queue if it is not empty.
+         *
+         * @return std::optional<event_entry> The first event in the queue, or an empty optional if the queue is empty.
+         */
         std::optional<event_entry> pop_first_event()
         {
             std::optional<event_entry> entry;
@@ -81,6 +118,13 @@ namespace tools
             return entry;
         }
 
+        /**
+         * @brief Pops the last event from the event queue.
+         *
+         * This function retrieves the last event from the event queue and clears the queue.
+         *
+         * @return std::optional<event_entry> The last event entry if the queue is not empty, otherwise std::nullopt.
+         */
         std::optional<event_entry> pop_last_event()
         {
             std::optional<event_entry> entry;
@@ -98,28 +142,56 @@ namespace tools
             return entry;
         }
 
+        /**
+         * @brief Checks if there are any events in the queue.
+         *
+         * @return true if the event queue is not empty, false otherwise.
+         */
         bool has_events()
         {
             return !m_evt_queue.empty();
         }
 
+        /**
+         * @brief Returns the number of events in the event queue.
+         *
+         * @return The number of events currently in the event queue.
+         */
         std::size_t number_of_events()
         {
             return m_evt_queue.size();
         }
 
+        /**
+         * @brief Waits for events by blocking until a signal is received.
+         */
         void wait_for_events()
         {
             m_wakeable.wait_for_signal();
         }
 
+        /**
+         * @brief Waits for events to occur within a specified timeout duration.
+         *
+         * This function blocks the calling thread until an event occurs or the specified timeout duration elapses.
+         *
+         * @param timeout The maximum duration to wait for events, specified as a std::chrono::duration with microsecond
+         * precision.
+         */
         void wait_for_events(const std::chrono::duration<std::uint64_t, std::micro>& timeout)
         {
             m_wakeable.wait_for_signal(timeout);
         }
 
     private:
+        /**
+         * @brief A synchronization object used for waking up threads or tasks.
+         */
         sync_object m_wakeable;
+
+        /**
+         * @brief A synchronized queue that holds tuples of Topic, Evt, and std::string.
+         */
         sync_queue<std::tuple<Topic, Evt, std::string>> m_evt_queue;
     };
 
