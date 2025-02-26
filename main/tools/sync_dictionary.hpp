e74f6da25182e9e4b312c4b063c198072a40c4c0
Laurent Lardinois <19912792+type-one@users.noreply.github.com>
2025-02-26 18:46:48 +0100

    docs: add doxygen doc to sync_dictionary

--- main/tools/sync_dictionary.hpp
+++ main/tools/sync_dictionary.hpp
@@ -1,3 +1,15 @@
+/**
+ * @file sync_dictionary.hpp
+ * @brief A thread-safe dictionary class for C++ Publish/Subscribe Pattern.
+ *
+ * This file contains the definition of the sync_dictionary class, which provides
+ * a thread-safe dictionary implementation using a mutex to protect access to the
+ * internal dictionary. It supports adding, removing, and retrieving key-value pairs,
+ * as well as checking the size and emptiness of the dictionary.
+ *
+ * @author Laurent Lardinois
+ * @date January 2025
+ */
 //-----------------------------------------------------------------------------//
 // C++ Publish/Subscribe Pattern - Spare time development for fun              //
 // (c) 2025 Laurent Lardinois https://be.linkedin.com/in/laurentlardinois      //
@@ -39,6 +51,17 @@
 
 namespace tools
 {
+    /**
+     * @brief A thread-safe dictionary class.
+     *
+     * This class provides a thread-safe dictionary implementation using a mutex
+     * to protect access to the internal dictionary. It supports adding, removing,
+     * and retrieving key-value pairs, as well as checking the size and emptiness
+     * of the dictionary.
+     *
+     * @tparam K The type of the keys in the dictionary.
+     * @tparam T The type of the values in the dictionary.
+     */
     template <typename K, typename T>
     class sync_dictionary : public non_copyable // NOLINT inherits from non copyable and non movable class
     {
@@ -50,18 +73,44 @@ namespace tools
             static constexpr bool value = true;
         };
 
+        /**
+         * @brief Adds a key-value pair to the dictionary.
+         *
+         * This method adds a new key-value pair to the dictionary. If the key already exists,
+         * its value will be updated with the new value provided.
+         *
+         * @param key The key to be added or updated in the dictionary.
+         * @param value The value associated with the key.
+         */
         void add(const K& key, const T& value)
         {
             std::lock_guard guard(m_mutex);
             m_dictionary[key] = value;
         }
 
+        /**
+         * @brief Removes the element with the specified key from the dictionary.
+         *
+         * This function acquires a lock to ensure thread safety while removing
+         * the element associated with the given key from the dictionary.
+         *
+         * @param key The key of the element to be removed.
+         */
         void remove(const K& key)
         {
             std::lock_guard guard(m_mutex);
             m_dictionary.erase(key);
         }
 
+        /**
+         * @brief Adds a collection of key-value pairs to the dictionary.
+         *
+         * This method takes a collection of key-value pairs and adds them to the
+         * internal dictionary. It uses a mutex to ensure thread safety during the
+         * operation.
+         *
+         * @param collection A map containing the key-value pairs to be added to the dictionary.
+         */
         void add_collection(const std::map<K, T> collection)
         {
             std::lock_guard guard(m_mutex);
@@ -71,6 +120,14 @@ namespace tools
             }
         }
 
+        /**
+         * @brief Adds an unordered collection of key-value pairs to the dictionary.
+         *
+         * This method takes an unordered map of key-value pairs and adds each pair to the dictionary.
+         * It uses a lock guard to ensure thread safety during the operation.
+         *
+         * @param collection The unordered map containing key-value pairs to be added to the dictionary.
+         */
         void add_collection(const std::unordered_map<K, T> collection)
         {
             std::lock_guard guard(m_mutex);
@@ -80,6 +137,14 @@ namespace tools
             }
         }
 
+        /**
+         * @brief Retrieves a snapshot of the current dictionary.
+         *
+         * This method returns a copy of the internal dictionary, ensuring thread safety
+         * by using a lock guard to protect the dictionary during the copy operation.
+         *
+         * @return A copy of the current dictionary.
+         */
         std::map<K, T> get_collection()
         {
             std::lock_guard guard(m_mutex);
@@ -87,6 +152,17 @@ namespace tools
             return snapshot;
         }
 
+        /**
+         * @brief Finds the value associated with the given key in the dictionary.
+         *
+         * This method searches for the specified key in the dictionary and returns
+         * the associated value if the key is found. If the key is not found, it
+         * returns an empty std::optional.
+         *
+         * @param key The key to search for in the dictionary.
+         * @return std::optional<T> The value associated with the key if found,
+         *         otherwise an empty std::optional.
+         */
         std::optional<T> find(const K& key)
         {
             std::optional<T> result;
@@ -99,18 +175,40 @@ namespace tools
             return result;
         }
 
+        /**
+         * @brief Checks if the dictionary is empty.
+         *
+         * This method acquires a lock on the mutex to ensure thread safety
+         * and then checks if the dictionary is empty.
+         *
+         * @return true if the dictionary is empty, false otherwise.
+         */
         bool empty()
         {
             std::lock_guard guard(m_mutex);
             return m_dictionary.empty();
         }
 
+        /**
+         * @brief Returns the number of elements in the dictionary.
+         *
+         * This method acquires a lock on the mutex to ensure thread safety
+         * while accessing the dictionary's size.
+         *
+         * @return The number of elements in the dictionary.
+         */
         std::size_t size()
         {
             std::lock_guard guard(m_mutex);
             return m_dictionary.size();
         }
 
+        /**
+         * @brief Clears all elements from the dictionary.
+         *
+         * This method acquires a lock on the mutex to ensure thread safety
+         * and then clears all elements from the dictionary.
+         */
         void clear()
         {
             std::lock_guard guard(m_mutex);
