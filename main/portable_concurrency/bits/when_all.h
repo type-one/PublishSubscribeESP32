b0a869f41ad342506730fd5ec2612a1ba249e378
Laurent Lardinois <19912792+type-one@users.noreply.github.com>
2026-04-23 23:10:46 +0200

    refactor: remove concurrency v1 api

--- main/portable_concurrency/bits/when_all.h
+++ main/portable_concurrency/bits/when_all.h
@@ -18,6 +18,10 @@
 
 #pragma once
 
+#if defined(PORTABLE_CONCURRENCY_V1_API_FROZEN) && !defined(PORTABLE_CONCURRENCY_ALLOW_V1_HEADERS)
+#error "portable_concurrency v1 when_all API is frozen. Migrate to result-based API in portable_concurrency/p_future_v2.hpp."
+#endif
+
 #include <atomic>
 #include <type_traits>
 #include <utility>
